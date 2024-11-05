e53ce972af1898db446144b40c9a355be1b1f3ca
asdasd7722 <psjune9707@naver.com>
2024-11-05 10:18:26 +0900

    feat: api 인사 기능 추가

--- src/main/java/hubtest/hubtest/Controller/HelloController.java
+++ src/main/java/hubtest/hubtest/Controller/HelloController.java
@@ -3,6 +3,8 @@ package hubtest.hubtest.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -12,4 +14,11 @@ public class HelloController {
         model.addAttribute("message", "이것은 내가 전달한 인사");
         return "hello-mvc";
     }
+
+    @GetMapping("thisisapi")
+    @ResponseBody
+    public String thisisapi(@RequestParam("name") String name) {
+        return "Hello " + name;
+    }
+
 }
