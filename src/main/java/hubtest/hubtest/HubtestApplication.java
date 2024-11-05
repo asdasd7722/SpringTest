d3c2c3c00e225a0b9c0abc37c31c4adfd73cbccf
asdasd7722 <psjune9707@naver.com>
2024-11-05 09:52:54 +0900

    first commit

---
+++ src/main/java/hubtest/hubtest/HubtestApplication.java
@@ -0,0 +1,13 @@
+package hubtest.hubtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hubtest/hubtest/HubtestApplicationTests.java
@@ -0,0 +1,13 @@
+package hubtest.hubtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
